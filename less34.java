5e94b5297d659ead17accee719c00d19db8952df
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-03-22 14:13:22 +0200

    безкінечний цикл

---
+++ less34.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class less34 {
+
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+while(true)
+        {
+            String s = input.nextLine();
+            if (s.equals("exit")) {
+                break;
+            }
+        }
+    }
+}
