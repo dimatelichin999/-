0d415af2f533de80956e2bc043f00d62030837a5
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-03-22 14:21:38 +0200

    цикли
    while+continue

--- less35.java
+++ less35.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class less35 {
+ class less35 {
+
+    public static void main(String args[]){
+
+        int x = 1;
+
+        while (x < 10){
+            x++;
+            if ((x % 7) == 0)
+                continue;
+            System.out.println(x);
+
+        }
+    }
 }
