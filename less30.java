0771b93ae969c4d698776ed30ce14517650bb558
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-03-22 11:56:42 +0200

    boolean

---
+++ less30.java
@@ -0,0 +1,14 @@
+class less30 {
+
+    public static void main(String args[]){
+
+        int x = 50;
+        boolean youngerAge = (x > 65);
+        if (youngerAge) {
+            System.out.println("Пора на пенсію");
+        }
+        else {
+            System.out.println("Потрібно ще працювати");
+        }
+    }
+}
