d6a5d9f81b202246addd9a759ddc2cfdc211d083
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-03-22 13:55:06 +0200

    while3

---
+++ less33.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class less33 {
+
+    public static void main(String args[]){
+
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt();
+
+        while(x==5){
+            System.out.println(5);
+        }
+    }
+}
