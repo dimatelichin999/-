2e64aac8ef09bfaa4d0f601bb2ea8ed0e1059670
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-03-22 13:08:15 +0200

    while
    1

---
+++ less31.java
@@ -0,0 +1,13 @@
+class less31 {
+
+    public static void main(String args[]){
+
+        int x = 5 ;
+
+        while (x <=  10) {
+            System.out.println(x);
+            x++;
+
+        }
+    }
+}
